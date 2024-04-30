d5f6931a7e148afcf92374bb0ebbeae5310fdf2c
jonOriginal <79869389+jonOriginal@users.noreply.github.com>
2024-05-01 11:00:48 +1200

    Update index.js

--- src/addon/src/index.js
+++ src/addon/src/index.js
@@ -5,8 +5,8 @@ import serverless from './serverless.js';
 const app = express();
 
 app.enable('trust proxy');
-app.use(express.static('static', { maxAge: '1y' }));
-app.use((req, res, next) => serverless(req, res, next));
+app.use('/knight', express.static('static', { maxAge: '1y' }));
+app.use('/knight', (req, res, next) => serverless(req, res, next));
 app.listen(process.env.PORT || 7000, () => {
   initBestTrackers()
       .then(() => console.log(`Started addon at: http://localhost:${process.env.PORT || 7000}`));
